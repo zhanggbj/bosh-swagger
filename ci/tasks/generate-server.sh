24b866ebd586177c1be46416377e1797bd76fb80
Gong Zhang <zhanggbj@cn.ibm.com>
2016-10-25 17:10:54 +0800

    debug ppl

--- ci/tasks/generate-server.sh
+++ ci/tasks/generate-server.sh
@@ -7,19 +7,19 @@ base_gopath=$( cd $base/../../../.. && pwd )
 go version
 export GOPATH=$base_gopath:$GOPATH
 echo "GOPATH=" $GOPATH
-ls
+pwd
 
 go get -u github.com/go-swagger/go-swagger/cmd/swagger
-go get -u github.com/kardianos/govendor
-govendor fetch github.com/go-openapi/errors
-govendor fetch github.com/go-openapi/loads
-govendor fetch github.com/go-openapi/runtime
-govendor fetch github.com/go-openapi/spec
-govendor fetch github.com/go-openapi/strfmt
-govendor fetch github.com/go-openapi/swag
-govendor fetch github.com/go-openapi/validate
-govendor fetch github.com/jessevdk/go-flags
-govendor fetch github.com/tylerb/graceful
+
+go get github.com/go-openapi/errors
+go get github.com/go-openapi/loads
+go get github.com/go-openapi/runtime
+go get github.com/go-openapi/spec
+go get github.com/go-openapi/strfmt
+go get github.com/go-openapi/swag
+go get github.com/go-openapi/validate
+go get github.com/jessevdk/go-flags
+go get github.com/tylerb/graceful
 
 cd src/github.com/cloudfoundry-community/bosh-softlayer-baremetal-server
 ../../../../bin/swagger validate swagger.json
@@ -28,4 +28,3 @@ cd src/github.com/cloudfoundry-community/bosh-softlayer-baremetal-server
 
 go build -o bin/bms cmd/soft-layer-baremetal-provisioning-server/main.go
 ./bin/bms --port 8080
-    
