b7fe8cf35f3c78df12dab918309b7325888d266f
Gong Zhang <zhanggbj@cn.ibm.com>
2016-10-25 18:27:27 +0800

    second init

--- ci/tasks/generate-server.sh
+++ ci/tasks/generate-server.sh
@@ -8,26 +8,25 @@ go version
 export GOPATH=$base_gopath:$GOPATH
 echo "GOPATH=" $GOPATH
 pwd
+pushd gopath/src/github.com/zhanggbj/bosh-swagger/
 
-go get -u github.com/go-swagger/go-swagger/cmd/swagger
-
-go get github.com/go-openapi/errors
-go get github.com/go-openapi/loads
-go get github.com/go-openapi/runtime
-go get github.com/go-openapi/spec
-go get github.com/go-openapi/strfmt
-go get github.com/go-openapi/swag
-go get github.com/go-openapi/validate
-go get github.com/jessevdk/go-flags
-go get github.com/tylerb/graceful
-
-pushd $base_gopath/src/github.com/zhanggbj/bosh-swagger/src/github.com/cloudfoundry-community/bosh-softlayer-baremetal-server
-
-echo "run ls"
-ls
-/../../../../../../../../bin/swagger validate swagger.json
-
-/../../../../../../../../bin/swagger generate server swagger.json
-
-go build -o bin/bms cmd/soft-layer-baremetal-provisioning-server/main.go
-./bin/bms --port 8080
+  go get -u github.com/go-swagger/go-swagger/cmd/swagger
+  ./bin/swagger validate docs/swagger/swagger.json
+  mkdir -p src/baremetal-provision-server
+  mv
+  pushd src/baremetal-provision-server
+    go get github.com/go-openapi/errors
+    go get github.com/go-openapi/loads
+    go get github.com/go-openapi/runtime
+    go get github.com/go-openapi/spec
+    go get github.com/go-openapi/strfmt
+    go get github.com/go-openapi/validate
+    go get github.com/jessevdk/go-flags
+    go get github.com/tylerb/graceful
+    go get github.com/gorilla/context
+    go get github.com/go-openapi/swag
+    ./../../bin/swagger generate server -f ./../../docs/swagger/swagger.json
+    go build -o bin/bms cmd/soft-layer-baremetal-provisioning-server/main.go
+    ls bin/
+  popd
+popd
