ae620df16ef3b733c0c8752b2434c01cc0d55bb6
Gong Zhang <zhanggbj@cn.ibm.com>
2016-10-26 14:43:05 +0800

    verify final

--- ci/tasks/generate-server.sh
+++ ci/tasks/generate-server.sh
@@ -10,11 +10,15 @@ echo "GOPATH=" $GOPATH
 echo "PWD is "$PWD
 version=`cat version-semver/number`
 server_name="baremetal-provision-server-"${version}
+divider="============================================"
 pushd gopath/src/github.com/zhanggbj/bosh-swagger/
   export GOPATH=$PWD
   go get -u github.com/go-swagger/go-swagger/cmd/swagger
   echo "ls"
   ls
+  echo ${divider}
+  echo "validate file swagger.json"
+  echo ${divider}
   ./bin/swagger validate docs/swagger/swagger.json
   mkdir -p src/${server_name}
   cp -r handlers src/${server_name}/
@@ -29,28 +33,38 @@ pushd gopath/src/github.com/zhanggbj/bosh-swagger/
     go get github.com/tylerb/graceful
     go get github.com/gorilla/context
     go get github.com/go-openapi/swag
+
+    echo ${divider}
+    echo "generate server"
+    echo ${divider}
     ./../../bin/swagger generate server -f ./../../docs/swagger/swagger.json
     #sed -i '/import/a "baremetal-provision-server\/handlers"' restapi/configure_soft_layer_baremetal_provisioning.go
     echo "PWD is" $PWD
 
+    echo ${divider}
     echo "update handlers..."
+    echo ${divider}
     ls handlers
     mv handlers/configure_soft_layer_baremetal_provisioning.go restapi/configure_soft_layer_baremetal_provisioning.go
     sed -i s/bosh-softlayer-baremetal-server/${server_name}/g restapi/configure_soft_layer_baremetal_provisioning.go
     sed -i s/bosh-softlayer-baremetal-server/${server_name}/g handlers/handlers.go
-#    cat restapi/configure_soft_layer_baremetal_provisioning.go
+    cat restapi/configure_soft_layer_baremetal_provisioning.go
 #    cat handlers/handlers.go
 
+    echo ${divider}
     echo "build baremetal provision server..."
+    echo ${divider}
 #    cat cmd/soft-layer-baremetal-provisioning-server/main.go
     go build -o bin/bms cmd/soft-layer-baremetal-provisioning-server/main.go
     ls bin/
 
+    echo ${divider}
     echo "verify bms..."
+    echo ${divider}
     ipaddr=`ifconfig | awk '/inet addr/{print substr($2,6)}' | sed -n 2p`
     nohup ./bin/bms --port 8080 --host ${ipaddr} &
     ps -ef|grep bms
-    curl http://${ipaddr}:8080/v1/info
+#    curl http://${ipaddr}:8080/v1/info
 
 #    echo "commit server..."
 
