6297fde9362b0f4e650c39e4fd71e409d8e03633
Gong Zhang <zhanggbj@cn.ibm.com>
2016-10-26 11:12:16 +0800

    add config file

--- ci/tasks/generate-server.sh
+++ ci/tasks/generate-server.sh
@@ -30,12 +30,13 @@ pushd gopath/src/github.com/zhanggbj/bosh-swagger/
     go get github.com/gorilla/context
     go get github.com/go-openapi/swag
     ./../../bin/swagger generate server -f ./../../docs/swagger/swagger.json
+    #sed -i '/import/a "baremetal-provision-server\/handlers"' restapi/configure_soft_layer_baremetal_provisioning.go
+    cp ../../handlers/configure_soft_layer_baremetal_provisioning.go restapi/configure_soft_layer_baremetal_provisioning.go
+    sed -i '/baremetal-provision-server/a ${server_name}' restapi/configure_soft_layer_baremetal_provisioning.go
+    cat restapi/configure_soft_layer_baremetal_provisioning.go
+    
     go build -o bin/bms cmd/soft-layer-baremetal-provisioning-server/main.go
     ls bin/
-
-    sed -i '/import/a "baremetal-provision-server\/handlers"' restapi/configure_soft_layer_baremetal_provisioning.go
-
-    cat restapi/configure_soft_layer_baremetal_provisioning.go
 
     git add models restapi cmd
     git config --global user.email zhanggbj@cn.ibm.com
