a1419ee97fef8c3a961ca883f72c9f2d954af6bd
zhanggbj <zhanggbj@cn.ibm.com>
2016-10-27 02:03:26 +0000

    generated baremetal-provision-server-0.0.174

---
+++ src/baremetal-provision-server-0.0.174/restapi/doc.go
@@ -0,0 +1,22 @@
+/*Package restapi SoftLayer Baremetal Provisioning
+
+BOSH SoftLayer baremetal provisioning (BMP) server APIs
+
+
+    Schemes:
+      http
+    Host: api.baremetal.softlayer.com
+    BasePath: /v1
+    Version: 0.2.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
